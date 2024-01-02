3da5a9254822836b52a6006fec957c9f33e57e94
10xvick <10xvick@gmail.com>
2024-01-03 00:39:56 +0530

    emit from client successfull

--- backend/server.ts
+++ backend/server.ts
@@ -1,42 +1,45 @@
-import { Socket } from 'socket.io';
-import { endpoints } from '../global/constants/endpoints';
-import { products } from './products';
-import { generate } from './utility';
+import { Socket } from "socket.io";
+import { endpoints } from "../global/constants/endpoints";
+import { products } from "./products";
+import { generate } from "./utility";
 
-const express = require('express');
-const {createServer} = require('node:http');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const { createServer } = require("node:http");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 const server = createServer(app);
-const io = require('socket.io')(server,{
-    cors: {
-        origin: "*", // Replace with the origin of your React app
-        methods: ["GET", "POST"]
-    }
-})
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*", // Replace with the origin of your React app
+    methods: ["GET", "POST"],
+  },
+});
 const port = endpoints.server.port;
 
+io.on("connection", (socket: Socket) => {
+  console.log("server was connected to client", socket.id);
 
+  socket.emit("connectx", socket.id + ", you have joined the server");
+  socket.broadcast.emit("connectx", "another client joined the server");
 
-io.on('connection',(socket:Socket)=>{
-    console.log('connected',socket.id)
+  socket.on("hello", (arg) => {
+    console.log("hello-from-client", arg);
+  });
 });
 
 // generate(app, products);
 
-
-app.get('/test', function (req, res) {
+app.get("/test", function (req, res) {
   setTimeout(function () {
     res.json({
-      data: 'server is running and accepting requests from this client',
+      data: "server is running and accepting requests from client",
     });
   }, 2000);
 });
 
-
 server.listen(port, function () {
-  return console.log('app is running on http://localhost:'.concat(`${port}`));
+  return console.log("app is running on http://localhost:".concat(`${port}`));
 });
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,48 @@
 import { useEffect, useState } from "react";
-import {endpoints} from "../../global/constants/endpoints"
+import { endpoints } from "../../global/constants/endpoints";
 
-import './index.css'
+import "./index.css";
 import { io } from "socket.io-client";
 const api = endpoints.server;
 const socket = io(endpoints.server.url);
 socket.connect();
 
-function App() {
-  const data = fetchData(api.url)
-  console.log(123,api)
+socket.on("connectx", (data) => {
+  console.log(data);
+});
 
+function App() {
+  const data = fetchData(api.url);
 
   return (
-    <> x x x a
-    
-    ayz d {data}
+    <>
+      {" "}
+      data:{data}
+      <button
+        onClick={(e) => {
+          console.log(e.target);
+          socket.emit("hello", { message: "world" });
+        }}
+      >
+        {" "}
+        test{" "}
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
 
-function fetchData(url:string){
-  const [data,setData] = useState(null);
-  useEffect(()=>{
-    fetch(url+'/test').then(e=>e.text()).then(e=>{
-      setData(e)
-    }).catch(e=>console.error(e))
-  },[]);
-  
-  return data; 
+function fetchData(url: string) {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch(url + "/test")
+      .then((e) => e.text())
+      .then((e: any) => {
+        setData(e);
+      })
+      .catch((e) => console.error(e));
+  }, []);
+
+  return data;
 }
